985e2719cb098059bd0f88f9247c9eb7784c3650
sayeed <expsayeedshorif@gmail.com>
2026-01-28 16:30:04 +0600

    jj

---
+++ components/mainPageComponents/Navbar.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='bg-foreground text-background font-social'>
+            <div></div>
+            
+        </div>
+    );
+};
+
+export default Navbar;
