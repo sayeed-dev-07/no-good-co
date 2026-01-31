59eb0461b1776d16cb73805b0a3267714e415ab9
sayeed <expsayeedshorif@gmail.com>
2026-01-31 19:56:39 +0600

    will be fixing this hover bug on the GiftImage

--- components/normalComponents/GiftImage.tsx
+++ components/normalComponents/GiftImage.tsx
@@ -56,24 +56,24 @@ const GiftImage = ({ index }: { index: number }) => {
         <div className='h-[75vh] sm:h-[90vh] overflow-hidden w-full relative'>
             <Image loading='lazy' data-speed="auto" fill src={data.imgSrc} className='gift-card-img object-cover min-h-[120%] scale-105 opacity-0 top-[-10%]' alt={`${data.shopName}-bgImg`} />
             <div className='w-full h-full absolute flex items-center justify-center z-10 inset-0'>
-                <div onClick={handleClick} onMouseEnter={handleHover} onMouseLeave={handleHoverOut} className='w-full max-w-80.5 cursor-pointer relative'>
-                    <div className='absolute z-20 inset-0 py-[8%] bg-[#dcc0b4] w-full rounded-4xl uppercase text-[13px] flex items-center justify-between px-[10%]'>
+                <div onClick={handleClick} onMouseEnter={handleHover} onMouseLeave={handleHoverOut} className='w-full max-w-80.5  relative'>
+                    <div  className='absolute cursor-pointer z-20 inset-0 py-[8%] bg-[#dcc0b4] w-full rounded-4xl uppercase text-[13px] flex items-center justify-between px-[10%]'>
                         <div className="dot h-2 w-2 bg-black rounded-full"></div>
                         <p>shop</p>
                         <p className='font-semibold'>{data.shopName}</p>
                         <FaAngleRight className='text-sm' />
                     </div>
-                    <div className='absolute  inset-0 rounded-4xl z-15  h-[250px] text-center overflow-hidden text-black w-full'>
-                        <div ref={animCardRef} className='h-full w-full bg-[#f3e8e4] -translate-y-full will-change-transform'>
+                    <div className='absolute  inset-0 rounded-4xl z-15  h-62.5 text-center overflow-hidden text-black w-full'>
+                        <div ref={animCardRef} className='h-full cursor-pointer w-full bg-[#f3e8e4] -translate-y-full will-change-transform'>
                         <div className='flex h-full py-2 items-end  justify-between w-full gap-x-1 text-[12px] uppercase'>
                             <div className='w-[50%]'>
-                                <div className='relative  h-[120px]'>
+                                <div className='relative  h-30'>
                                     <Image fill sizes='100vw' alt={`${data.shopName}-proImg1`} className='object-contain' src={data.pro1Img} />
                                 </div>
                                 <p>{data.pro1Text}</p>
                             </div>
                             <div className='w-[50%]'>
-                                <div className='relative  h-[120px]'>
+                                <div className='relative  h-30'>
                                     <Image fill sizes='100vw' alt={`${data.shopName}-proImg2`} className='object-contain' src={data.pro2Img} />
                                 </div>
                                 <p>{data.pro2Text}</p>
