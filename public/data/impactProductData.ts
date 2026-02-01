bb0c6b06eea5da50762ebc04cbbcfc50e0ead30f
sayeed <expsayeedshorif@gmail.com>
2026-02-01 10:57:59 +0600

    added the store data

---
+++ public/data/impactProductData.ts
@@ -0,0 +1,22 @@
+export const impactData = [
+    {
+        text: 'Sweet Memories Cookbook',
+        price:'$750',
+        imgLink:'/images/productImg1.png'
+    },
+    {
+        text: 'Pepe Saya x No Good Nostalgia Ceramics Set',
+        price:'$1000',
+        imgLink:'/images/productImg2.png'
+    },
+    {
+        text: 'Hunter x No Good Togetherness At Our Table Set',
+        price:'$420',
+        imgLink:'/images/productImg3.png'
+    },
+    {
+        text: 'Dinosaur Designs x No Good Always Celebrate With Cake Set',
+        price:'$580',
+        imgLink:'/images/productImg4.png'
+    },
+]
